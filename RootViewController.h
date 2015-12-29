f605a243cc3a46873e26f2967008be4513f712e8
孙林鹏 <sun_lin_peng@163.com>
2015-12-29 11:42:11 +0800

    first

---
+++ RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  test
+//
+//  Created by 孙林鹏 on 15/12/28.
+//  Copyright © 2015年 jinzhaoshenghui.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *text1;
+@property (weak, nonatomic) IBOutlet UITextField *text2;
+
+@end
---
+++ RootViewController.m
@@ -0,0 +1,40 @@
+//
+//  RootViewController.m
+//  test
+//
+//  Created by 孙林鹏 on 15/12/28.
+//  Copyright © 2015年 jinzhaoshenghui.com. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc]initWithIdentifier:@"xxxxxx" accessGroup:nil];
+    self.text1.text = [wrapper objectForKey:(id)kSecAttrAccount];
+//    self.text2.text = [wrapper objectForKey:(id)kSecValueData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
