d2cf972be1879b6f7b42fbfd62ebe1c6178e1ec6
孙林鹏 <sun_lin_peng@163.com>
2015-12-29 11:46:32 +0800

    cece

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 孙林鹏 on 15/12/28.
+//  Copyright © 2015年 jinzhaoshenghui.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 孙林鹏 on 15/12/28.
+//  Copyright © 2015年 jinzhaoshenghui.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
